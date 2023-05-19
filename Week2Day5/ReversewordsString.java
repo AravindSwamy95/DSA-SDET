be3ece96c44904ae037e48b50315445f3b7171d8
Aravind Swamy <aravindsvb@gmail.com>
2023-05-19 08:01:50 +0530

    Add files via upload

---
+++ Week2Day5/ReversewordsString.java
@@ -0,0 +1,26 @@
+package DSA;
+
+
+/*
+ * Write a program to reverse each word in a String
+Example:
+Input: Welcome To Testleaf
+Output: emocleW oT fealtesT
+ */
+public class ReversewordsString {
+
+	
+	
+	
+	public void reversewordsString(String s) {
+		
+	
+		
+		int left=0; int right=s.length()-1;
+		
+		while(left<right) {
+		if()
+		}
+		
+	}
+}
