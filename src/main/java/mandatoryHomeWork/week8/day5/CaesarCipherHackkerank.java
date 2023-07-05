6dc230f4ff4634f9f75c26e9491cf0efdf83c443
Aravind Swamy <aravindsvb@gmail.com>
2023-07-05 15:43:51 +0530

    Add files via upload

---
+++ src/main/java/mandatoryHomeWork/week8/day5/CaesarCipherHackkerank.java
@@ -0,0 +1,43 @@
+mmmmmmpackage DSA;
+
+import org.junit.Test;
+
+public class CaesarCipherHackkerank {
+	@Test
+	public void test1() {
+		String s = "middle-Outz";
+		int shift =2;
+		System.out.println(caesarCipher(s, shift));
+	}
+	
+	    public static String caesarCipher(String s, int k) {
+	        StringBuilder encrypted = new StringBuilder();
+
+	        for (char c : s.toCharArray()) {
+	            if (Character.isLetter(c)) {
+	            	char base;
+	            	if(Character.isUpperCase(c) ) {
+	            		base = 'A';
+	            	}
+	            	else {
+	            		base = 'a';
+	            	}
+	          
+	                int originalAlphabetPosition = c - base;
+	                int newAlphabetPosition = (originalAlphabetPosition + k) % 26;
+	                char newCharacter = (char) (base + newAlphabetPosition);
+	                encrypted.append(newCharacter);
+	            } else {
+	                encrypted.append(c);
+	            }
+	        }
+
+	        return encrypted.toString();
+	    }
+
+
+	    
+	}
+
+
+
